30d27cd987867f67aa39d1e8b14ba3470ebcf069
Abdullah Mzaien <xa300600@hotmail.com>
2022-04-01 18:29:27 +0300

    chore: updating remix/nextui and adding eslint

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,13 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-module.exports = {
+ module.exports = {
   serverBuildTarget: "cloudflare-pages",
   server: "./server.js",
   devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "functions/[[path]].js",
   // publicPath: "/build/",
-  // devServerPort: 8002
 };
